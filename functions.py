31bff1049d7d37ebe390b86189ab6756c79621fc
praveenrajaram25 <praveenrajaa2000@gmail.com>
2023-02-04 23:40:53 +0530

    functions in python

---
+++ functions.py
@@ -0,0 +1,65 @@
+# def even(num):
+#     if(num%2==0):
+#         return num
+#     else:
+#         pass
+#
+# #number = int(input("enter number :  "))
+#
+# n_list=[1,2,3,4,5]
+# for i in n_list:
+#     if i%2==0:
+#         result = even(i)
+#         print(result)
+
+
+# work_hours=[('sai',10),('praveen',7),('aadhi',6)]
+#
+#
+# def emp_year(work_hours):
+#     cur_max_hrs = 0
+#     employee = ''
+#     for (emp, emp_hrs) in work_hours:
+#         if (emp_hrs > cur_max_hrs):
+#             cur_max_hrs = emp_hrs
+#             employee = emp
+#
+#     return (employee,cur_max_hrs)
+#
+# res=emp_year(work_hours)
+# print("employee of the year : ",res[0], "\n the work hours are : ",res[1])
+
+#
+# def shopping(**inputs):
+#     #for i in inputs:
+#     return inputs
+#
+#
+# # shop_dict=
+# res=shopping(fruit='apple')
+# print(res)
+
+# def myfunc(*lst):
+#     final_list=[i for i in lst if i%2==0]
+#     return final_list
+#
+# res=myfunc(1,2,3,4)
+# print(res)
+#
+
+# def myfunc(string):
+#     op=[]
+#     for i in range(len(string)):
+#         if i%2==0:
+#             op.append(string[i].lower())
+#         else:
+#             op.append(string[i].upper())
+#     return op
+# print(myfunc('abcf'))
+
+# text='hai how are you'
+# lst=[1,2,3]
+# lst.reverse()
+# print(lst)
+
+
